c6961f87b3f02317d063e5786fc0f4d8defa16f6
Henrique Grolli Bassotto <henrique.grolli@gmail.com>
2012-10-25 23:44:59 -0200

    Full number validation with ranges

---
+++ src/net/guax/jorderly/json/Range.java
@@ -0,0 +1,10 @@
+package net.guax.jorderly.json;
+
+import java.math.BigDecimal;
+
+public class Range {
+
+    public BigDecimal lowerBound = null;
+    public BigDecimal upperBound = null;
+    
+}
